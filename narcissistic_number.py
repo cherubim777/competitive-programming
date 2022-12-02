c93c54329060bd5e9f99dbc673c38777531f7a92
Leburik <77838958+cherubim777@users.noreply.github.com>
2022-12-02 10:39:54 +0300

    Update narcissistic_number.py
    more effective one-liner using the sum function

--- narcissistic_number.py
+++ narcissistic_number.py
@@ -1,16 +1,2 @@
 def narcissistic( value ):
-    digit = len(str(value))
-    sum = 0
-    # place holder for the value and digit that will decrease
-    # in order of magnitude as the loop goes
-    intermediate_value = value  
-    intermediate_digit = digit
-    while intermediate_value > 0:
-        # degree of the value is 1 less of to intermidiate_digit the will be raised to
-        degree = 10**(intermediate_digit-1)
-        current_digit_value = intermediate_value  // degree
-        sum += current_digit_value ** digit
-        intermediate_value -= degree* current_digit_value
-        intermediate_digit -= 1
-    return sum == value
-
+   return value == sum(int(x) ** len(str(value)) for x in str(value))
